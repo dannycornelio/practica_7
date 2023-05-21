ca0c428c77b6f10761f282866bf73c81bc402c3f
dannycornelio <licdannycornelio@gmail.com>
2023-05-21 16:23:19 -0600

    primer arrego

---
+++ controladores/datos/alumnos.php
@@ -0,0 +1,50 @@
+<?php
+require '../../modelos/datos_alumnos.php';
+
+try {
+    $producto = new Producto($_POST);
+    $resultado = $producto->guardar();
+    $error = "NO se guardó correctamente";
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2){
+    $error = $e2->getMessage();
+}
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Estado del Formulario</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if($resultado): ?>
+                    <div class="alert alert-success" role="alert">
+                        Los Datos fueron guardados exitosamente!
+                    </div>
+                <?php else :?>
+                    <div class="alert alert-danger" role="alert">
+                        Los Datos no pudieron ser guardados correctamente: <?= $error ?>
+                    </div>
+                <?php endif ?>
+              
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="/practica_7/vistas/datos/index.php" class="btn btn-info">Volver al formulario</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
